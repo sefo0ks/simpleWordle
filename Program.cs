d16101aaddcf4d90f076fe90d950fcb73ecea16b
sefo0ks <44124840+sefo0ks@users.noreply.github.com>
2022-07-30 20:26:25 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@
         List<string> words = new();
         string wordToGuess;
 
+        List<char> usedCharacters = new();
+
         List<string> previousTries = new();
         int guessesRemain;
 
@@ -21,13 +23,13 @@
         bool run = true;
         while (run)
         {
-            ClearConsoleAndShowWords(guessesRemain, previousTries, wordToGuess);
+            ClearConsoleAndShowWords(guessesRemain, previousTries, wordToGuess, usedCharacters);
             
-            guess = GetInput(previousTries, words);
+            guess = GetInput(previousTries, words, usedCharacters, guessesRemain, wordToGuess);
             previousTries.Add(guess);
             guessesRemain--;
 
-            HandleEndGame(guess, wordToGuess, previousTries, guessesRemain);
+            HandleEndGame(guess, wordToGuess, previousTries, guessesRemain, usedCharacters);
         }
     }
 
@@ -64,42 +66,51 @@
         _guessesRemain = MaxGuesses;
     }
 
-    private static void ClearConsoleAndShowWords(int _guessesRemain, List<string> _previousTries, string _wordToGuess)
+    private static void ClearConsoleAndShowWords(int _guessesRemain, List<string> _previousTries, 
+                                                 string _wordToGuess, List<char> _usedCharacters)
     {
         Console.Clear();
 
         Console.WriteLine("Guesses remain: " + _guessesRemain);
         for (int i = 0; i < MaxGuesses; i++)
+        {
+            if (i <  6 - _guessesRemain)
             {
-                if (i <  6 - _guessesRemain)
+                Console.Write($"{i + 1}: ");
+                for (int j = 0; j < _previousTries[i].Length; j++)
                 {
-                    Console.Write($"{i + 1}: ");
-                    for (int j = 0; j < _previousTries[i].Length; j++)
-                    {
-                        char currentChar = _previousTries[i][j];
+                    char currentChar = _previousTries[i][j];
 
-                        if (_wordToGuess[j] == _previousTries[i][j])
-                            Console.BackgroundColor = inPositionColor;
-                        else if (_wordToGuess.Contains(currentChar))
-                            Console.BackgroundColor = inWordColor;
-                        else
-                            Console.BackgroundColor = BackgroundColor;
+                    if (_wordToGuess[j] == _previousTries[i][j])
+                        Console.BackgroundColor = inPositionColor;
+                    else if (_wordToGuess.Contains(currentChar))
+                        Console.BackgroundColor = inWordColor;
+                    else
+                        Console.BackgroundColor = BackgroundColor;
 
-                        Console.Write(currentChar);
+                    Console.Write(currentChar);
 
-                        if (j != _previousTries[i].Length - 1)
-                            Console.Write(" ");
-                    }
-                    Console.BackgroundColor = BackgroundColor;
-                    Console.WriteLine();
+                    if (j != _previousTries[i].Length - 1)
+                        Console.Write(" ");
                 }
-                else
-                    Console.WriteLine($"{i + 1}: _ _ _ _ _");
+                Console.BackgroundColor = BackgroundColor;
+                Console.WriteLine();
             }
+            else
+                Console.WriteLine($"{i + 1}: _ _ _ _ _");
+        }
+
+        Console.Write("Guessed letters: ");
+        foreach (char ch in _usedCharacters)
+        {
+            Console.Write(ch + " ");
+        }
+        Console.WriteLine();
     }
 
-    private static string GetInput(List<string> _previousTries, List<string> _allWords)
+    private static string GetInput(List<string> _previousTries, List<string> _allWords, List<char> _usedCharacters, int _guessesRemain, string _wordToGuess)
     {
+        ClearConsoleAndShowWords(_guessesRemain, _previousTries, _wordToGuess, _usedCharacters);
         Console.Write("Your guess: ");
         string input = Console.ReadLine();
         input = input.ToUpper();
@@ -109,46 +120,60 @@
             if (Char.IsLetter(ch) == false)
             {
                 Console.WriteLine("Wrong character(-s) passed!");
-                input = GetInput(_previousTries, _allWords);
+                Console.WriteLine("press any key to continue...");
+                Console.ReadKey();
+                input = GetInput(_previousTries, _allWords, _usedCharacters, _guessesRemain, _wordToGuess);
             }
         }
 
         if (input.Length != 5)
         {
             Console.WriteLine("Write 5 characters long words only!");
-            input = GetInput(_previousTries, _allWords);
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+            input = GetInput(_previousTries, _allWords, _usedCharacters, _guessesRemain, _wordToGuess);
         }
         
         if (_previousTries.Any(prevTry => prevTry.ToUpper() == input))
         {
             Console.WriteLine("You tried this word already.");
-            input = GetInput(_previousTries, _allWords);
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+            input = GetInput(_previousTries, _allWords, _usedCharacters, _guessesRemain, _wordToGuess);
         }
 
         if (_allWords.Any(word => word.ToUpper() == input))
-            return input;
+        {}
         else
         {
             Console.WriteLine($"Word {input} is not valid");
-            input = GetInput(_previousTries, _allWords);
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+            input = GetInput(_previousTries, _allWords, _usedCharacters, _guessesRemain, _wordToGuess);
         }
 
+        foreach (char ch in input)
+            if (!_usedCharacters.Contains(ch))
+                _usedCharacters.Add(ch);
+        
+        _usedCharacters.Sort();
         return input;
     }
 
-    private static void HandleEndGame(string _guess, string _wordToGuess, List<string> _previousTries, int _guessesRemain)
+    private static void HandleEndGame(string _guess, string _wordToGuess, List<string> _previousTries,
+                                      int _guessesRemain, List<char> _usedCharacters)
     {
         bool gameEnded = false;
 
         if (_guess.ToUpper() == _wordToGuess.ToUpper())
         {
-            ClearConsoleAndShowWords(_guessesRemain, _previousTries, _wordToGuess);
+            ClearConsoleAndShowWords(_guessesRemain, _previousTries, _wordToGuess, _usedCharacters);
             Console.WriteLine("YOU WON!!!");
             gameEnded = true;
         }
         else if (_guessesRemain < 1)
         {
-            ClearConsoleAndShowWords(_guessesRemain, _previousTries, _wordToGuess);
+            ClearConsoleAndShowWords(_guessesRemain, _previousTries, _wordToGuess, _usedCharacters);
             Console.WriteLine($"Guessed word was - {_wordToGuess}");
             Console.WriteLine("YOU LOST!!!");
             gameEnded = true;
